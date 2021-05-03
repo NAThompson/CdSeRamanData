295f51efdb8f0619af9c7c744ae1f0bbaca08bb2
Nick Thompson <4nt@ornl.gov>
2021-05-03 15:38:25 -0400

    Begin translation to Julia.

---
+++ julia/fit_parameters.jl
@@ -0,0 +1,14 @@
+# Raman spectra pipeline for Brian Lerner and Ben Lawrie.
+# Written by Nick Thompson, but follows Brian Lerner's python script.
+
+using Plots
+using PlotThemes
+theme(:solarized)
+PLOTS_DEFAULTS = Dict(:dpi => 600)
+Plots.GRBackend()
+
+function main()
+    print("Yo")
+end
+
+main()
